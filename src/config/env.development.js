eab6d8a5fa7c50bb8fca36593f6f713845a7a057
allen <allen.li@decathlon.com>
2020-09-16 15:19:58 +0800

    appId update by env

--- src/config/env.development.js
+++ src/config/env.development.js
@@ -1,5 +1,6 @@
 // 本地配置
 module.exports = {
     baseUrl: 'http://localhost:9022/',
-    api: 'http://localhost:9022/api/v1/'
+    api: 'http://localhost:9022/api/v1/',
+    appId: '188_development',
 }
